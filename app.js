5fa3ca980c44ccad7317fb50f0660c19099cd194
bazilakbar <bazilakbar4@gmail.com>
2024-04-05 22:44:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.write("<h1>Rules for naming JavaScript Variables</h1>" +
+  "<br><br>A variable name can contain only letters, numbers, $ and _ . For example $my_firstVariable" +
+  "<br>Variable must begin with a letter, $ or _name, name" +
+  "<br>Variables names are case sensitive" +
+  "<br>Variables names should not be JS keywords");
